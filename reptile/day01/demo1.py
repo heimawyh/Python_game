cc13f969a539d459961dbe7350dfaef83a081054
heimawyh <wyh2104109693@163.com>
2022-08-24 00:30:00 +0800

    新建爬虫项目体系结束更新

--- reptile/day01/demo1.py
+++ reptile/day01/demo1.py
@@ -1,18 +1,28 @@
 import urllib.request
 import requests
 
+
 url = "https://www.bilibili.com"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36"
+}
 
 
 # 访问
 def use_urllib(url):
-    response = urllib.request.urlopen(url)
+    req = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(req)
     data = response.read().decode()
     print(data)
+
+
 def use_requests(url):
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     data = response.text
-    print(data)
+    # print(data)
+    filePath = "bilibili.html"
+    with open(filePath,"w",encoding="utf-8")as f:
+        f.write(data)
 
 if __name__ == '__main__':
     # use_urllib(url)
